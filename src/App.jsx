d96400ecf4e56f7e89ecfc38dcee0d013212ac46
Alec Mirambeau <alecmirambeau@gmail.com>
2026-03-05 16:29:34 -0500

    Application before breaking it into components and adding functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App(props) {
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1 hidden={false}>TodoMatic</h1>
       <form>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
